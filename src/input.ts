b7dc4ebf0c78ffba0982f75a53d230f48c2a76e9
Ross <rosssullivan101@gmail.com>
2024-07-08 09:14:45 -0700

    Added support for `working-directory` (#21)

--- src/input.ts
+++ src/input.ts
@@ -8,11 +8,13 @@ import { input } from '@clechasseur/rs-actions-core';
 export interface Input {
     token: string;
     ignore: string[];
+    workingDirectory: string;
 }
 
 export function get(): Input {
     return {
         token: input.getInput('token', { required: true }),
         ignore: input.getInputList('ignore', { required: false }),
+        workingDirectory: input.getInput('working-directory', { required: false }) ?? '.',
     };
 }
