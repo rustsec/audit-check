rustsec/audit-check

commit b7dc4ebf0c78ffba0982f75a53d230f48c2a76e9

Added support for `working-directory` (#21)

src/input.ts
@@ -8,11 +8,13 @@ import { input } from '@clechasseur/rs-actions-core';
 export interface Input {
     token: string;
     ignore: string[];
+    workingDirectory: string;
 }
 
 export function get(): Input {
     return {
         token: input.getInput('token', { required: true }),
         ignore: input.getInputList('ignore', { required: false }),
+        workingDirectory: input.getInput('working-directory', { required: false }) ?? '.',
     };
 }

src/main.ts
@@ -12,6 +12,7 @@ import * as reporter from './reporter';
 
 async function getData(
     ignore: string[] | undefined,
+    workingDirectory: string,
 ): Promise<interfaces.Report> {
     const cargo = await Cargo.get();
     await cargo.findOrInstall('cargo-audit');
@@ -24,6 +25,7 @@ async function getData(
             commandArray.push('--ignore', item);
         }
         commandArray.push('--json');
+        commandArray.push('--file', `${workingDirectory}/Cargo.lock`);
         await cargo.call(commandArray, {
             ignoreReturnCode: true,
             listeners: {
@@ -44,9 +46,17 @@ async function getData(
     return JSON.parse(stdout);
 }
 
+function removeTrailingSlash(str) {
+    if (str[str.length - 1] === '/') {
+        return str.substr(0, str.length - 1);
+    }
+    return str;
+}
+
 export async function run(actionInput: input.Input): Promise<void> {
     const ignore = actionInput.ignore;
-    const report = await getData(ignore);
+    const workingDirectory = removeTrailingSlash(actionInput.workingDirectory);
+    const report = await getData(ignore, workingDirectory);
     let shouldReport = false;
     if (!report.vulnerabilities.found) {
         core.info('No vulnerabilities were found');

action.yml
@@ -11,6 +11,10 @@ inputs:
   ignore:
     description: Comma-separated list of advisory ids to ignore
     required: false
+  working-directory:
+    description: The directory of the Cargo.toml / Cargo.lock files to scan.
+    required: false
+    default: .
 
 runs:
   using: 'node20'

commit 7e5e97a5ec1530ef6eb2a5a33a8d859733e1bf68

fix: do not fail when Cargo.lock is not found
Fix a regression introduced by `working-directory` settings, introduced by commit b7dc4eb.

The commit started to invoke `cargo-audit` with the `--file <working-dir>/Cargo.lock` flag.
However not all the Rust projects have `Cargo.lock` files committed; take
libraries as an example.

This commit changes the `working-directory` default value to be an empty string.
In this way the `--file` flag can be added only when the user actually provides
this parameter.

Finally, the code has been changed to build the final path to the `Cargo.lock`
file in a more robust way. The prior code assumed the action would be run on a
unix system. It would have failed on a Windows machine.

Signed-off-by: Flavio Castelli <fcastelli@suse.com>

src/input.ts
@@ -15,6 +15,6 @@ export function get(): Input {
     return {
         token: input.getInput('token', { required: true }),
         ignore: input.getInputList('ignore', { required: false }),
-        workingDirectory: input.getInput('working-directory', { required: false }) ?? '.',
+        workingDirectory: input.getInput('working-directory', { required: false }) ?? '',
     };
 }

src/main.ts
@@ -1,5 +1,6 @@
 import * as process from 'process';
 import * as os from 'os';
+import * as path from 'path';
 
 import * as core from '@actions/core';
 import * as github from '@actions/github';
@@ -25,7 +26,9 @@ async function getData(
             commandArray.push('--ignore', item);
         }
         commandArray.push('--json');
-        commandArray.push('--file', `${workingDirectory}/Cargo.lock`);
+        if (workingDirectory != '') {
+            commandArray.push('--file', path.join(workingDirectory, 'Cargo.lock'));
+        }
         await cargo.call(commandArray, {
             ignoreReturnCode: true,
             listeners: {